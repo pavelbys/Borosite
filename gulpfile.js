5ac5eff2d5f59f47323abce3a97fdf5175476347
William Livernois <wliverno@mit.edu>
2018-02-15 17:50:13 -0500

    Modified gulpfile.js to allow /documents/ access.

--- gulpfile.js
+++ gulpfile.js
@@ -56,6 +56,7 @@ gulp.task('minify-html', function() {
 
 // Copy any remaining files (images and javascript libraries) that dont need minification
 gulp.task('copy-files', function () {
+	gulp.src('public/documents/**/*').pipe(gulp.dest('dist/documents/'));
 	gulp.src('public/images/**/*').pipe(gulp.dest('dist/images/'));
 	gulp.src('public/js/libraries/**/*').pipe(gulp.dest('dist/js/libraries/'));
 });
